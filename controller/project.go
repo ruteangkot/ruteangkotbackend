427104906f12ab1263ab6139a029d0fb4daee2ac
Rolly Maulana Awangga <awangga@gmail.com>
2024-05-24 16:47:00 +0700

    GetDataProject

--- controller/project.go
+++ controller/project.go
@@ -66,3 +66,33 @@ func PostDataProject(respw http.ResponseWriter, req *http.Request) {
 	}
 
 }
+
+func GetDataProject(respw http.ResponseWriter, req *http.Request) {
+	payload, err := watoken.Decode(config.PublicKeyWhatsAuth, helper.GetLoginFromHeader(req))
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error : Token Tidak Valid"
+		respn.Info = helper.GetSecretFromHeader(req)
+		respn.Location = "Decode Token Error"
+		respn.Response = err.Error()
+		helper.WriteJSON(respw, http.StatusForbidden, respn)
+		return
+	}
+	docuser, err := atdb.GetOneDoc[model.Userdomyikado](config.Mongoconn, "user", primitive.M{"phonenumber": payload.Id})
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error : Data user tidak di temukan"
+		respn.Response = err.Error()
+		helper.WriteJSON(respw, http.StatusNotImplemented, respn)
+		return
+	}
+	existingprjs, err := atdb.GetAllDoc[[]model.Project](config.Mongoconn, "project", primitive.M{"owner._id": docuser.ID})
+	if err != nil {
+		var respn model.Response
+		respn.Status = "Error : Data project tidak di temukan"
+		respn.Response = err.Error()
+		helper.WriteJSON(respw, http.StatusNotFound, respn)
+		return
+	}
+	helper.WriteJSON(respw, http.StatusOK, existingprjs)
+}
