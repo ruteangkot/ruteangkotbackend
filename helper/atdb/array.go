31cbd677d3523686afafa1e7ea4c87415bf299fb
Rolly Maulana Awangga <awangga@gmail.com>
2024-05-24 18:04:24 +0700

    add to array and delete from array

---
+++ helper/atdb/array.go
@@ -0,0 +1,52 @@
+package atdb
+
+import (
+	"context"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func AddDocToArray[T any](collection *mongo.Collection, ID string, arrayname string, newDoc T) (result *mongo.UpdateResult, err error) {
+	ObjectID, err := primitive.ObjectIDFromHex(ID)
+	if err != nil {
+		return
+	}
+
+	filter := bson.M{"_id": ObjectID}
+	update := bson.M{
+		"$push": bson.M{arrayname: newDoc},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	result, err = collection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return
+	}
+	return
+}
+
+func DeleteDocFromArray[T any](collection *mongo.Collection, ID string, arrayname string, Doc T) (result *mongo.UpdateResult, err error) {
+	ObjectID, err := primitive.ObjectIDFromHex(ID)
+	if err != nil {
+		return
+	}
+
+	filter := bson.M{"_id": ObjectID}
+	update := bson.M{
+		"$pull": bson.M{arrayname: Doc},
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	result, err = collection.UpdateOne(ctx, filter, update)
+	if err != nil {
+		return
+	}
+	return
+}
