ff67c80c7745e656446acc44258471642786b543
RexndleZero <raihanaditya1506@gmail.com>
2024-05-28 13:23:49 +0700

    update #3

--- config/cors.go
+++ config/cors.go
@@ -3,16 +3,9 @@ package config
 import "net/http"
 
 var Origins = []string{
-	"https://naskah.bukupedia.co.id",
-	"https://auth.ulbi.ac.id",
-	"https://sip.ulbi.ac.id",
-	"https://euis.ulbi.ac.id",
-	"https://home.ulbi.ac.id",
-	"https://alpha.ulbi.ac.id",
-	"https://dias.ulbi.ac.id",
-	"https://iteung.ulbi.ac.id",
-	"https://whatsauth.github.io",
-	"https://www.do.my.id",
+	"https://ruteangkot.github.io/",
+	"htps://auth.ulbi.ac.id",
+	
 }
 
 var Headers = []string{
@@ -34,13 +27,13 @@ func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
 		w.Header().Set("Access-Control-Allow-Methods", "POST,GET")
-		w.Header().Set("Access-Control-Allow-Origin", "https://www.do.my.id")
+		w.Header().Set("Access-Control-Allow-Origin", "https://ruteangkot.github.io")
 		w.Header().Set("Access-Control-Max-Age", "3600")
 		w.WriteHeader(http.StatusNoContent)
 		return true
 	}
 	// Set CORS headers for the main request.
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
-	w.Header().Set("Access-Control-Allow-Origin", "https://www.do.my.id")
+	w.Header().Set("Access-Control-Allow-Origin", "https://ruteangkot.github.io")
 	return false
 }
