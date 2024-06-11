bfbe15184b8f82963373fdcc39abd741346a694f
RexndleZero <raihanaditya1506@gmail.com>
2024-06-11 15:41:18 +0700

    Backend||| Menambahkan omitempty di dalam struct ruteangkot

--- model/ruteangkot.go
+++ model/ruteangkot.go
@@ -4,7 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type RuteAngkot struct {
 	ID             primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	Rute           string             `bson:"Rute" json:"Rute"`
-	JamOperasional string             `bson:"Jam Operasional" json:"Jam Operasional"`
-	Tarif          string             `bson:"Tarif" json:"Tarif"`
+	Rute           string             `bson:"Rute,omitempty" json:"Rute,omitempty"`
+	JamOperasional string             `bson:"Jam Operasional,omitempty" json:"Jam Operasional,omitempty"`
+	Tarif          string             `bson:"Tarif,omitempty" json:"Tarif,omitempty"`
 }
