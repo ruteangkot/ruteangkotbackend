fe5ee1f9b88be6933ccb714bebdc80542d6116d2
RexndleZero <raihanaditya1506@gmail.com>
2024-06-11 15:11:03 +0700

    Backend||| Update ruteangkot.go menambahkan variable dt

--- controller/ruteangkot.go
+++ controller/ruteangkot.go
@@ -1,10 +1,12 @@
 package controller
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gocroot/config"
 	"github.com/gocroot/helper"
+	"github.com/gocroot/helper/at"
 	"github.com/gocroot/helper/atdb"
 	"github.com/gocroot/model"
 	"go.mongodb.org/mongo-driver/bson"
@@ -23,8 +25,32 @@ func CreateRoute(respw http.ResponseWriter, req *http.Request) {
 }
 
 func UpdateRoute(respw http.ResponseWriter, req *http.Request) {
-	resp, _:= atdb.GetAllDoc[[]model.RuteAngkot](config.Mongoconn, "data json", bson.M{})
-	helper.WriteJSON(respw, http.StatusOK, resp)
+	var rute model.RuteAngkot
+	err := json.NewDecoder(req.Body).Decode(&rute)
+	if err != nil {
+		var respn model.Response
+		respn.Response = err.Error()
+		at.WriteJSON(respw, http.StatusForbidden, respn)
+		return
+	}
+	dt, err:= atdb.GetOneDoc[model.RuteAngkot](config.Mongoconn,"data json",bson.M{"_id":rute.ID})
+	if err != nil {
+		var respn model.Response
+		respn.Response = err.Error()
+		at.WriteJSON(respw, http.StatusForbidden, respn)
+		return
+	}
+	dt.JamOperasional = rute.JamOperasional
+	dt.Rute = rute.Rute
+	dt.Tarif = rute.Tarif
+	_, err= atdb.ReplaceOneDoc(config.Mongoconn,"data json",bson.M{"_id":rute.ID},dt)
+	if err != nil {
+		var respn model.Response
+		respn.Response = err.Error()
+		at.WriteJSON(respw, http.StatusForbidden, respn)
+		return
+	}
+	helper.WriteJSON(respw, http.StatusOK, dt)
 	
 }
 
---
+++ helper/at/nethttp.go
@@ -0,0 +1,44 @@
+package at
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func GetSecretFromHeader(r *http.Request) (secret string) {
+	if r.Header.Get("secret") != "" {
+		secret = r.Header.Get("secret")
+	} else if r.Header.Get("Secret") != "" {
+		secret = r.Header.Get("Secret")
+	}
+	return
+}
+
+func GetLoginFromHeader(r *http.Request) (secret string) {
+	if r.Header.Get("login") != "" {
+		secret = r.Header.Get("login")
+	} else if r.Header.Get("Login") != "" {
+		secret = r.Header.Get("Login")
+	}
+	return
+}
+
+func Jsonstr(strc interface{}) string {
+	jsonData, err := json.Marshal(strc)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(jsonData)
+}
+
+func WriteJSON(respw http.ResponseWriter, statusCode int, content interface{}) {
+	respw.Header().Set("Content-Type", "application/json")
+	respw.WriteHeader(statusCode)
+	respw.Write([]byte(Jsonstr(content)))
+}
+
+func WriteString(respw http.ResponseWriter, statusCode int, content string) {
+	respw.WriteHeader(statusCode)
+	respw.Write([]byte(content))
+}
