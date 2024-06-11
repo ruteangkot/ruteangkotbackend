89f21c47f632c4e908938f2a51c75df03c5111b2
RexndleZero <raihanaditya1506@gmail.com>
2024-06-11 14:20:35 +0700

    Backend||| Update cors.go menambahkan didalam method PUT dan DELETE

--- config/cors.go
+++ config/cors.go
@@ -26,7 +26,7 @@ func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
 	if r.Method == http.MethodOptions {
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type,Login")
-		w.Header().Set("Access-Control-Allow-Methods", "POST,GET")
+		w.Header().Set("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE")
 		w.Header().Set("Access-Control-Allow-Origin", "https://ruteangkot.github.io")
 		w.Header().Set("Access-Control-Max-Age", "3600")
 		w.WriteHeader(http.StatusNoContent)
@@ -35,6 +35,6 @@ func SetAccessControlHeaders(w http.ResponseWriter, r *http.Request) bool {
 	// Set CORS headers for the main request.
 	w.Header().Set("Access-Control-Allow-Credentials", "true")
 	w.Header().Set("Access-Control-Allow-Origin", "https://ruteangkot.github.io")
-	w.Header().Set("Access-Control-Allow-Methods", "POST,GET")
+	w.Header().Set("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE")
 	return false
 }
