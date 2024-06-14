d285d5020ee4b1c044bf168446d91cf5a6b5a4bb
RexndleZero <raihanaditya1506@gmail.com>
2024-06-14 23:22:15 +0700

    Backend||| menambah function di dalam main.go

--- main.go
+++ main.go
@@ -1,11 +1,56 @@
 package gocroot
 
 import (
-	"github.com/gocroot/route"
+	"encoding/json"
+	"log"
+	"net/http"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/gocroot/route"
+	"github.com/gorilla/mux"
+	"gopkg.in/mgo.v2"
 )
 
 func init() {
 	functions.HTTP("WebHook", route.URL)
+}
+
+var session *mgo.Session
+
+func init() {
+    var err error
+    session, err = mgo.Dial("localhost")
+    if err != nil {
+        log.Fatalf("Failed to connect to MongoDB: %v", err)
+    }
+    session.SetMode(mgo.Monotonic, true)
+}
+
+func registerUser(w http.ResponseWriter, r *http.Request) {
+    var user mgo.User
+    err := json.NewDecoder(r.Body).Decode(&user)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    collection := session.DB("userDB").C("users")
+    err = collection.Insert(user)
+    if err != nil {
+        http.Error(w, "Error registering new user.", http.StatusInternalServerError)
+        return
+    }
+
+    log.Printf("New user registered: %+v", user) // Logging data yang disimpan
+
+    w.WriteHeader(http.StatusOK)
+    w.Write([]byte("User registered successfully!"))
+}
+
+func main() {
+    router := mux.NewRouter()
+    router.HandleFunc("/register", registerUser).Methods("POST")
+
+    log.Println("Server started at :3000")
+    log.Fatal(http.ListenAndServe(":3000", router))
 }
