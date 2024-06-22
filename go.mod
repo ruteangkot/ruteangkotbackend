6d846103baaa5e30ae4ad54f3706fe990d254c07
RexndleZero <raihanaditya1506@gmail.com>
2024-06-23 00:54:32 +0700

    Backend||| menambahkan function createunique index di config dan menambahkan function main

--- go.mod
+++ go.mod
@@ -6,8 +6,10 @@ require (
 	aidanwoods.dev/go-paseto v1.5.1
 	github.com/GoogleCloudPlatform/functions-framework-go v1.8.1
 	github.com/go-playground/validator/v10 v10.22.0
+	github.com/gorilla/mux v1.8.1
 	go.mongodb.org/mongo-driver v1.15.0
 	golang.org/x/crypto v0.23.0
+	gopkg.in/mgo.v2 v2.0.0-20190816093944-a6b53ec6cb22
 )
 
 require (
--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"log"
+	"net/http"
+
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"github.com/gocroot/config"
+	"github.com/gocroot/controller"
 	"github.com/gocroot/route"
+	"github.com/gorilla/mux"
 )
 
 func init() {
     functions.HTTP("WebHook", route.URL)
 }
 
+func main() {
+    // Connect to the database and set up the index
+    config.ConnectDB()
+
+    r := mux.NewRouter()
+    r.HandleFunc("/register", controller.Register).Methods("POST")
+    r.HandleFunc("/login", controller.Login).Methods("POST")
+
+    log.Fatal(http.ListenAndServe(":8080", r))
+}
