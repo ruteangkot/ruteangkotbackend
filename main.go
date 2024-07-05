468335a097b36b5ff0fe31d963d2f08f6c899385
RexndleZero <raihanaditya1506@gmail.com>
2024-07-06 01:05:48 +0700

    Menambahkan function main di main.go

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package gocroot
 
 import (
+	"net/http"
+
 	"github.com/gocroot/config"
+	"github.com/gocroot/controller"
 	"github.com/gocroot/route"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
@@ -10,4 +13,8 @@ import (
 func init() {
 	config.InitDB()
 	functions.HTTP("WebHook", route.URL)
+}
+func main() {
+	http.HandleFunc("/request-reset-password", controller.RequestResetPassword)
+	http.HandleFunc("/reset-password", controller.ResetPassword)
 }
