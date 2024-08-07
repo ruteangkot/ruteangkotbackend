5a7b5c03b0491a594228dc3e4bbeb298b7c5591f
Aditkece <147034263+RexndleZero@users.noreply.github.com>
2024-08-07 19:59:06 +0700

    Update db.go

--- config/db.go
+++ config/db.go
@@ -21,23 +21,7 @@ var mongoinfo = model.DBInfo{
 
 var Mongoconn, ErrorMongoconn = helper.MongoConnect(mongoinfo)
 
-var DB *mongo.Database
 
-func InitDB() {
-	clientOptions := options.Client().ApplyURI("mongodb+srv://Angkutankotabdg:naikangkot001@cluster02.hqmgzeb.mongodb.net/")
-	client, err := mongo.NewClient(clientOptions)
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	defer cancel()
 
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
 
-	DB = client.Database("Angkutankotabdg")
-	log.Println("Connected to MongoDB!")
-}
